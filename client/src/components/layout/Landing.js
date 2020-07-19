6a26ed37bf1174c45d30eee08b8f4e709a20d2bb
Kartik Soni <kartiksoni2209@gmail.com>
2020-07-19 18:15:39 +0530

    initial commit

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">
+            Welcome to DevelopersIn - LinkedIn for Developers
+          </h1>
+          <p className="lead">
+            Create a developer profile/portfolio, share posts and get help from
+            other developers
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+        <p className="lead">Copyright Kartik Soni 2020</p>
+      </div>
+    </section>
+  );
+};
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);
